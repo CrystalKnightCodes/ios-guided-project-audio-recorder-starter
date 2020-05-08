b261ec1a496bf572a1bb7cec4f4c2eace8e7e4cf
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-05-07 22:56:27 -0400

    Added the time remaining to the UI.

--- SimpleAudioRecorder/AudioRecorderController.swift
+++ SimpleAudioRecorder/AudioRecorderController.swift
@@ -59,12 +59,15 @@ class AudioRecorderController: UIViewController {
         
         let elapsedTime = audioPlayer?.currentTime ?? 0
         let duration = audioPlayer?.duration ?? 0
+        let timeRemaining = duration.rounded() - elapsedTime
         
         timeElapsedLabel.text = timeIntervalFormatter.string(from: elapsedTime)
+        timeRemainingLabel.text = "-" + timeIntervalFormatter.string(from: timeRemaining)!
         
         timeSlider.minimumValue = 0
         timeSlider.maximumValue = Float(duration)
         timeSlider.value = Float(elapsedTime)
+        
     }
     
     deinit {
